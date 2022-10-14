e6265d50c9b0432c311e45ca521fa06c09c63253
Sedhuraman07 <113281609+Sedhuraman07@users.noreply.github.com>
2022-10-14 12:04:34 +0530

    Add files via upload

---
+++ assignment/Sedhuraman S/assignment 2/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
+
+@app.route("/login")
+def signin():
+    return render_template('login.html')
+
+@app.route("/success")
+def success():
+    return render_template('success.html')
